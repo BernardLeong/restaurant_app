4215ec3ba5c44df6daf9ad01ec1ce273dbe50788
Bernard <bernardleongqijie@gmail.com>
2018-02-13 10:43:46 +0800

    add restaurant index action and view for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # For details on the DSL available within the, see http://guides.rubyonrails.org/routing.html
+  
+  # resources :restaurants, only: [:index , :showis fil]
+  root "restaurants#index"
+
+
+  # namespace :admin do
+
+  #   root 'restaurants#index'
+  # end
+
+
 end
